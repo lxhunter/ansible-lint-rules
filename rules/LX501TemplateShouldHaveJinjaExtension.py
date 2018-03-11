943566bbd117fdc769a489572b33c733557def83
Alexander Jaeger <alexander.jaeger@me.com>
2018-03-11 17:33:18 +0100

    fix: LX501TemplateShouldHaveJinjaExtension handles vars now

--- rules/LX501TemplateShouldHaveJinjaExtension.py
+++ rules/LX501TemplateShouldHaveJinjaExtension.py
@@ -10,7 +10,7 @@ class LX501TemplateShouldHaveJinjaExtension(AnsibleLintRule):
     def matchtask(self, file, task):
         if task['action']['__ansible_module__'] != 'template':
             return False
-        if task['action']['src'] == '{{ item }}':
+        if task['action']['src'].startswith('{{'):
             return False
         ext = os.path.splitext(task['action']['src'])
         if ext[1] != ".j2":
